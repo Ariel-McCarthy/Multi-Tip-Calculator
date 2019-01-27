d6bb89de5338e78e6622d73f0a3903551a9da583
Ariel-McCarthy <somccarthy@csumb.edu>
2019-01-27 13:28:47 -0800

    First commit, all basic tip calculation functionality added

---
+++ Multi Tip Calculator/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Multi Tip Calculator
+//
+//  Created by Ariel McCarthy on 1/26/19.
+//  Copyright © 2019 Sovreign-Ariel McCarthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func OnTap(_ sender: Any)
+    {
+        print("Hello")
+        
+        view.endEditing(true)
+    }
+    
+    @IBAction func CalculateTip(_ sender: Any)
+    {
+        //get bill amt
+        let bill = Double(billField.text!) ?? 0
+        
+        //calc tip and total
+        let tipPercentage = [0.1, 0.18, 0.2]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update tip/total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
